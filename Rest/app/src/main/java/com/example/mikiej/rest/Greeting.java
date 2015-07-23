ca07aa5ab14df0f707f872ae0dd5e86c9700e755
mikiejones <mikiej_boy@hotmail.com>
2015-07-23 09:20:15 +0100

    initial rest connection
    Following along the lines of spring tutorial - managed to connect the
    app to my web service.
    
    Initial project setup then add labels to display return greeting

---
+++ Rest/app/src/main/java/com/example/mikiej/rest/Greeting.java
@@ -0,0 +1,19 @@
+package com.example.mikiej.rest;
+
+/**
+ * Created by mikieJ on 21/07/2015.
+ */
+public class Greeting {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+}
---
+++ Rest/app/src/main/java/com/example/mikiej/rest/User.java
@@ -0,0 +1,7 @@
+package com.example.mikiej.rest;
+
+/**
+ * Created by mikieJ on 23/07/2015.
+ */
+public class User {
+}
