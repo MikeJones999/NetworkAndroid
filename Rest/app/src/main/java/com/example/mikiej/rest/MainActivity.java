50a4e22ca0a43a169ab69d32623db49eba245c07
mikiejones <mikiej_boy@hotmail.com>
2015-07-24 00:43:39 +0100

    Added new buttons and send strings to url
    Added text fields to type in, added submit button and added a new click
    on method within MainActivity that activates the HttpRequestTask method
    to send the rest request to the server. the method name is in the button
    setup and relays directly to this method
    
    It takes the string username and password and send it via post in the
    url.
    
    tested the connection - makes a connect accepts the password and then
    shows the user.

--- Rest/app/src/main/java/com/example/mikiej/rest/MainActivity.java
+++ Rest/app/src/main/java/com/example/mikiej/rest/MainActivity.java
@@ -5,35 +5,73 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends ActionBarActivity {
+
+
+
+    //variables for holding userName and Password & button objects
+    EditText userName;
+    EditText password;
+    Button ButtonSendUserDetails;
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+
+
     }
+
+
 
     @Override
     protected void onStart() {
         super.onStart();
-        new HttpRequestTask().execute();
+        //new HttpRequestTask().execute();
     }
 
     @Override
@@ -75,13 +113,52 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
+
+//    private class HttpRequestTask extends AsyncTask<Void, Void, User> {
+//        @Override
+//        protected User doInBackground(Void... params) {
+//            try {
+//
+//                TextView userNameLabel = (TextView) findViewById(R.id.id_value);
+//
+//                final String url = "http://192.168.0.19:8080/HomeNetwork/greeting?name=mj&password=mj@123";
+//                RestTemplate restTemplate = new RestTemplate();
+//                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+////                Greeting greeting = restTemplate.getForObject(url, Greeting.class);
+////                return greeting;
+//
+//                User user = restTemplate.getForObject(url, User.class);
+//                return user;
+//
+//            } catch (Exception e) {
+//                Log.e("MainActivity", e.getMessage(), e);
+//            }
+//
+//            return null;
+//        }
+//protected void onPostExecute(User user) {
+//    TextView greetingIdText = (TextView) findViewById(R.id.id_value);
+//    TextView greetingContentText = (TextView) findViewById(R.id.content_value);
+//    greetingIdText.setText(user.getUserName());
+//    greetingContentText.setText(user.getUserRole());
+//
+//}
+//
+//}
+
+
+
     private class HttpRequestTask extends AsyncTask<Void, Void, User> {
         @Override
         protected User doInBackground(Void... params) {
+
+            String username = userName.getText().toString();
+
+            String userPassword = password.getText().toString();
+
+            //final String url = "http://192.168.0.19:8080/HomeNetwork/greeting?name=" + username + "&password=" + userPassword;
             try {
-                BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-                String encodeduserPassword = passwordEncoder.encode(returnedUser.getPassword());
-                final String url = "http://192.168.0.19:8080/HomeNetwork/greeting?name=mj&password=mj@123";
+                final String url = "http://192.168.0.19:8080/HomeNetwork/greeting?name=" + username + "&password=" + userPassword;
                 RestTemplate restTemplate = new RestTemplate();
                 restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
 //                Greeting greeting = restTemplate.getForObject(url, Greeting.class);
@@ -97,13 +174,6 @@ public class MainActivity extends ActionBarActivity {
             return null;
         }
 
-//        @Override
-//        protected void onPostExecute(Greeting greeting) {
-//            TextView greetingIdText = (TextView) findViewById(R.id.id_value);
-//            TextView greetingContentText = (TextView) findViewById(R.id.content_value);
-//            greetingIdText.setText(greeting.getId());
-//            greetingContentText.setText(greeting.getContent());
-//        }
 
 
         protected void onPostExecute(User user) {
@@ -111,8 +181,44 @@ public class MainActivity extends ActionBarActivity {
             TextView greetingContentText = (TextView) findViewById(R.id.content_value);
             greetingIdText.setText(user.getUserName());
             greetingContentText.setText(user.getUserRole());
+
         }
 
     }
+
+
+
+
+    public void sendUserDetails(View Button)
+    {
+
+        //Get userName from text input
+        userName = (EditText) findViewById(R.id.userName);
+        //Get password from text input
+        password = (EditText) findViewById(R.id.password);
+        //create button object
+        ButtonSendUserDetails = (Button) findViewById(R.id.ButtonSendUserDetails);
+//        String username = userName.getText().toString();
+//
+//        String userPassword = password.getText().toString();
+
+        //final String url = "http://192.168.0.19:8080/HomeNetwork/greeting?name=" + username + "&password=" + userPassword;
+        //final String url = "http://192.168.0.19:8080/HomeNetwork/greeting";
+        //check whether the msg empty or not
+//        if(username.length()>0 && userPassword.length()>0) {
+
+
+
+//                userName.setText(""); //reset the name text field
+//                password.setText(""); //reset the password text field
+                new HttpRequestTask().execute();
+
+//        } else {
+//            //display message if text field is empty
+//            Toast.makeText(getBaseContext(),"All fields are required",Toast.LENGTH_SHORT).show();
+//            //return null;
+//        }
+    }
+
 
 }
